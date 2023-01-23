dcd5f58802bfe252b83c3162ce4d9eeebbcf940f
Cole Medin <47287758+coleam00@users.noreply.github.com>
2023-01-23 10:42:50 -0600

    Moved OBURN to BSC

--- scripts/move_tokens_to_bsc.py
+++ scripts/move_tokens_to_bsc.py
@@ -6,28 +6,24 @@ from web3 import Web3
 import math
 import csv
 
-# TBURN address on BSC
-TBURN_ADDRESS = "0x005dAd50D38245f764C3C4f127E72acB9b5ebb6B"
 # OBURN address on BSC
-OBURN_ADDRESS = "0xdbb1b2226528bc4751a91000df4ee938886d660e"
+OBURN_ADDRESS = "0x76E45C89254907bA5dd6558be3Dd78fD0A0320F3"
 
 SET_BALANCES = True
 
-def move_tokens_to_bsc(tburnAddress=None, oburnAddress=None):
+def move_tokens_to_bsc(oburnAddress=None):
     account = get_account()
 
     currNetwork = network.show_active()
 
-    if not tburnAddress:
-        tburnAddress = TBURN_ADDRESS
-
     if not oburnAddress:
         oburnAddress = OBURN_ADDRESS
 
-    TBURN = interface.IERC20(tburnAddress)
     OBURN = interface.IERC20(oburnAddress)
 
     print(f"Account BNB balance is currently: {account.balance()}")
+
+    totalBalance = 0
 
     for token in ["TBURN", "OBURN"]:
         accounts = []
@@ -41,25 +37,25 @@ def move_tokens_to_bsc(tburnAddress=None, oburnAddress=None):
             for holder in holderData:
                 if holder[0] != "HolderAddress":
                     accounts.append(holder[0])
-                    balances.append(int(float(holder[1])))
-                    tokenBalance += int(float(holder[1]))
+                    balances.append(float(holder[1]))
+                    tokenBalance += float(holder[1])
 
         assert len(accounts) == len(balances)
         
         if SET_BALANCES:
             for i in range(len(accounts)):
-                if token == "TBURN":
-                    print(f"Sending {Web3.toWei(balances[i], 'ether')} TBURN to {accounts[i]}")
-                    # TBURN.transfer(accounts[i], Web3.toWei(balances[i], "ether"), {"from": account})
-                elif token == "OBURN":
-                    print(f"Sending {Web3.toWei(balances[i], 'ether')} OBURN to {accounts[i]}")
-                    # OBURN.transfer(accounts[i], Web3.toWei(balances[i], "ether"), {"from": account})
+                print(f"Sending {Web3.toWei(balances[i], 'ether')} OBURN to {accounts[i]}")
+                OBURN.transfer(accounts[i], Web3.toWei(balances[i], "ether"), {"from": account})
         else:
             print(f"\nHolder information for {token}:\n")
             # print(accounts)
             # print(balances)
             print(f"Number of holders: {len(accounts)}")
             print(f"Sum of all account balances for {token} is: {sum(balances)}")
+            totalBalance += sum(balances)
+
+    if not SET_BALANCES:
+        print(f"Final total balance is: {totalBalance}")
 
     print(f"\nAccount BNB balance is currently: {account.balance()}")
 
